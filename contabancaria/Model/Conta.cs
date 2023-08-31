1d9add7a288858db49b872f8a35470df87e74d14
dValsc <valeria.dcarvalho@hotmail.com>
2023-08-31 14:02:21 -0300

    model conta

---
+++ contabancaria/Model/Conta.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace contabancaria.Model
+{
+    public class Conta
+    {
+        private int id;
+        private int agencia;
+        private int tipo;
+        private string titular = string.Empty;
+        private decimal saldo;
+
+        public Conta(int numero, int agencia, int tipo, string titular, decimal saldo)
+        {
+            this.id = numero;
+            this.agencia = agencia;
+            this.tipo = tipo;
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+
+        /*Métodos Get e Set*/
+        public int GetNumero()
+        {
+            return numero;
+        }
+
+        public void SetNumero(int numero)
+        {
+            this.numero = numero;
+        }
+
+        public int GetAgencia()
+        {
+            return agencia;
+        }
+
+        public void SetAgencia(int agencia)
+        {
+            this.agencia = agencia;
+        }
+
+        public int GetTipo()
+        {
+            return tipo;
+        }
+
+        public void SetTipo(int tipo)
+        {
+            this.tipo = tipo;
+        }
+
+        public string GetTitular()
+        {
+            return titular;
+        }
+
+        public void SetTitular(string titular)
+        {
+            this.titular = titular;
+        }
+
+        public decimal GetSaldo()
+        {
+            return saldo;
+        }
+
+        public void SetSaldo(decimal saldo)
+        {
+            this.saldo = saldo;
+        }
+    }
+}
